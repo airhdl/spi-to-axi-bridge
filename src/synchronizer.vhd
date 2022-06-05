ab602bec2e6ee72f1f782f1c366b2931d6f5a15e
Guy Eschemann <guy@noasic.com>
2022-06-05 18:02:03 +0200

    update synchronizer

--- src/synchronizer.vhd
+++ src/synchronizer.vhd
@@ -1,5 +1,4 @@
 -------------------------------------------------------------------------------
---
 --  Synchronizer for clock-domain crossings.
 --
 --  This file is part of the noasic library.
@@ -9,34 +8,22 @@
 --    to a destination clock domain using a chain of flip-flops (synchronizer
 --    FF followed by one or more guard FFs).
 --
---  See also:
---    * http://noasic.com/blog/how-not-to-design-a-2dff-synchronizer/
---
 --  Author(s):
 --    Guy Eschemann, Guy.Eschemann@gmail.com
---
 -------------------------------------------------------------------------------
---
---  Copyright (c) 2012 Guy Eschemann
---
---  This source file may be used and distributed without restriction provided
---  that this copyright statement is not removed from the file and that any
---  derivative work contains the original copyright notice and the associated
---  disclaimer.
---
---  This source file is free software: you can redistribute it and/or modify it
---  under the terms of the GNU Lesser General Public License as published by
---  the Free Software Foundation, either version 3 of the License, or (at your
---  option) any later version.
---
---  This source file is distributed in the hope that it will be useful, but
---  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
---  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
---  for more details.
---
---  You should have received a copy of the GNU Lesser General Public License
---  along with the noasic library.  If not, see http://www.gnu.org/licenses
---
+-- Copyright (c) 2012-2022 Guy Eschemann
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--     http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 -------------------------------------------------------------------------------
 
 library ieee;
